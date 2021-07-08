8103f80e9d772140bd7f04be394a7ed15a532824
Brandon Craig <bcraig2612@gmail.com>
2021-07-08 15:10:40 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {Typography} from "@material-ui/core";
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Typography>
+        Special Offers
+      </Typography>
+    </React.Fragment>
+  );
+}
